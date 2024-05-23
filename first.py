96781066666a2ee8400a1c32d444542ba0224dc2
nishantgujjar1152 <nishnatbhati1152@gmail.com>
2024-05-23 15:12:30 +0530

    first file

---
+++ first.py
@@ -0,0 +1,32 @@
+#a = 5
+#print(a)
+
+""" num1 = 4
+num2 = 5
+
+sum = num1 + num2
+print('Total sum value: '+str(sum)) """
+
+a = 15
+b = 16
+sum = a + b
+print(sum)
+print('total sum value: '+str(sum)) 
+c = 16
+d = 15 
+subtract = a - b
+print(subtract)
+print('total subtraction value; '+str(subtract))
+e = 2
+f = 3 
+multiply = e * f
+print(multiply)
+print ('total multiply value: '+str(multiply))
+a = 4
+b = 2
+divide = a / b
+print(divide)
+print('total divide value: '+str(divide))
+
+
+
